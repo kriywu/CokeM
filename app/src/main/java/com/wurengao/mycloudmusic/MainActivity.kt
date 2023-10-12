8ce1de9448412a235fac3118edba39b87f863074
wurengao <wurengao@bytedance.com>
2023-10-12 21:27:14 +0800

    fix: add splash page

--- app/src/main/java/com/wurengao/mycloudmusic/MainActivity.kt
+++ app/src/main/java/com/wurengao/mycloudmusic/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.wurengao.mycloudmusic
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val year = Calendar.getInstance()[Calendar.YEAR]
+        findViewById<TextView>(R.id.copyright).text = getString(R.string.copyright, year)
     }
 }
